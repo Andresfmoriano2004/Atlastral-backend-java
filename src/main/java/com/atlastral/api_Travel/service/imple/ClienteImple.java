52bce0a2e983a2568f3f0a2d9e467765ffa2ef83
Andres F Moriano <morianoa970@gmail.com>
2026-03-27 02:22:13 -0500

    fix: servicios agregados

--- src/main/java/com/atlastral/api_Travel/service/imple/ClienteImple.java
+++ src/main/java/com/atlastral/api_Travel/service/imple/ClienteImple.java
@@ -3,10 +3,14 @@ package com.atlastral.api_Travel.service.imple;
 import com.atlastral.api_Travel.models.dao.ClienteDao;
 import com.atlastral.api_Travel.models.entity.Cliente;
 import com.atlastral.api_Travel.service.Icliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 
 public class ClienteImple  implements Icliente {
 
-
+    @Autowired
     private ClienteDao ClienteDao;
 
     @Override
